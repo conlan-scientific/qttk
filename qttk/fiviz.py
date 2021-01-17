aafac36befc56a6c1afada48d57fac2814e2515a
joe wojniak <joe.wojniak@gmail.com>
2021-01-17 15:56:13 -0700

    RSI algorithm passed validation

--- qttk/fiviz.py
+++ qttk/fiviz.py
@@ -4,24 +4,37 @@
 # fiviz.py - Financial Visualization
 # candles chart with rsi
 
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import datetime
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import os
 from profiler import time_this
 
 def fiviz(x, y1, y2):
-    fig, axs = plt.subplots(2, 1)
-    axs[0].plot(x, y1)
-    axs[0].set_ylabel('Close')
+    fig, axs = plt.subplots(2, 1, figsize=(10,6))
+    plt.subplots_adjust(hspace=0.3)
+    locator = mdates.AutoDateLocator(minticks=5, maxticks=10)
+    formatter = mdates.ConciseDateFormatter(locator)
+
+    axs[0].set_title('SPY OHLC Price')
+    axs[0].xaxis.set_major_locator(locator)
+    axs[0].xaxis.set_major_formatter(formatter)
+    # transpose data for boxplots (i.e. candle plots)
+    y1_transposed = y1.T
+    axs[0].boxplot(y1_transposed, whis='range')
+    axs[0].set_ylabel('Price')
     axs[0].grid(True)
 
+    axs[1].set_title('SPY RSI')
+    axs[1].xaxis.set_major_locator(locator)
+    axs[1].xaxis.set_major_formatter(formatter)
     axs[1].plot(x, y2)
     axs[1].set_xlabel('Date')
     axs[1].set_ylabel('RSI')
     axs[1].grid(True)
 
-    fig.tight_layout()
     plt.show()
 
 @time_this
@@ -51,16 +64,26 @@ def net_returns(df):
 
 @time_this
 def moving_average(df_slice, window):
+    # 2021-01-17v3 moving_average algorithm - correctly validates vs. Excel
     i = window
-    new_index = df_slice.index.tolist()
-    mvgAvg = pd.DataFrame(np.nan, index=new_index, columns=[0])
-    while i < len(df_slice)+1:
+    mvgAvg = pd.DataFrame(0.0, index=np.arange(len(df_slice)), columns=[0])
+    while i < len(df_slice) + 1:
         window_start = i - window
-        j = i - 1  # row index reference
-        window_end = j
-        mvgAvg.iloc[j] = np.sum(df_slice[window_start:window_end])/window
+        window_end = i         # numpy doesn't select the ending index in a slice
+        j = window_end - 1     # row index reference
+        try:
+            mvgAvg.iloc[j] = np.sum(df_slice[window_start:window_end])/window
+        except:
+            mvgAvg.iloc[j] = 0.0
         i = i + 1
     return mvgAvg
+
+@time_this
+def save_data(filename, df):
+    # save_data is a convenience method to save data to .csv file
+    # df needs to be a Pandas dataframe
+    filename = path+'\data\validation_data\{}.csv'.format(filename)
+    df.to_csv(filename)
 
 @time_this
 def rsi(rets, window):
@@ -85,15 +108,19 @@ def rsi(rets, window):
 
     up = rets.loc[rets.iloc[:]>=0]
     up = up.reindex(date_range, fill_value=0)
+    #save_data('up', up)
     up_avg = moving_average(up, window)
     up_avg = up_avg.fillna(value=0)
+    #save_data('up_avg', up_avg)
 
     down = rets.loc[rets.iloc[:]<0]
     down = down.reindex(date_range, fill_value=0)
+    #save_data('down', down)
     down_avg = moving_average(down, window)*-1
     down_avg = down_avg.fillna(value=0)
-    # replace 0s with last non-zero value
-    down_avg.replace(to_replace=0, method='ffill')
+    # replace 0s with 1s
+    down_avg.replace(to_replace=0, value=1)
+    #save_data('down_avg', down_avg)
 
     # calculate rsi
     rs = up_avg/down_avg
@@ -104,8 +131,16 @@ if __name__ == '__main__':
     path = os.path.dirname(os.getcwd())
     filename = path+'\data\SPY.csv'
     data = pd.read_csv(filename, index_col=0, parse_dates=True)
+    # window needs to be optimized for trading frequency/price autocorrelation lag period
     window = 14
     rets_SPY = net_returns(data)
+    # save_data() instances are for validating the RSI algorithm
+    # comment out/remove save_data() instances once RSI algorithm is
+    # validated.
+    # RSI algorithm validated against Excel: 2021-01-17v3
+    #save_data('rets_SPY', rets_SPY)
     rsi_SPY = rsi(rets_SPY, window)
-    x = -window*3
-    fiviz(data.index[x:], data['close'].iloc[x:], rsi_SPY.iloc[x:])
+    #save_data('rsi_SPY', rsi_SPY)
+    x = -window*3                  # define the date range for fiviz to plot
+    price = data[['open', 'close', 'low', 'high']]
+    fiviz(data.index[x:], price.iloc[x:], rsi_SPY.iloc[x:])
