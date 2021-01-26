7b071220854a68c1e3ca83fcb3d8c324d1465d9f
emican <=>
2021-01-25 19:08:05 -0500

    available indicators as of Jan 25, 2021

--- qttk/indicators.py
+++ qttk/indicators.py
@@ -1,5 +1,21 @@
+'''
+Indicators Interface
+  rsi
+  bollinger
+  ma
+  ema
+  wma
+'''
+from qttk.fiviz import rsi as rsi
+from qttk.Bollinger_1 import bollinger as bollinger
+from qttk.mvgAvg import mvgAvg2 as ma
+from qttk.testema import exponential_moving_average_v2 as ema
+from qttk.testwma import weighted_moving_avg_v3 as wma
 
-from qttk.rsi import compute_rsi_v4 as compute_rsi
+
+
+
+
 
 
 
