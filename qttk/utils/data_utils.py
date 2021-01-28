c7b320a09202ad4d5ad41340ed6d1f521144c07e
emican <=>
2021-01-28 16:40:46 -0500

    #8 refactor data_utils.py to use our testing convention.  Delete tests/data_utils.ut.py

--- qttk/utils/data_utils.py
+++ qttk/utils/data_utils.py
@@ -31,4 +31,49 @@ def check_dataframe_columns(dataframe: pd.DataFrame,
         column_difference = required_columns[~column_test]
         missing_column = ', '.join(column_difference.values)
         required_columnsf = ', '.join(required_columns)
-        raise IndexError(f"Required: {required_columnsf} Missing: {missing_column}")
+        error_message = (f" Missing: {missing_column} "
+                        f"Required: {required_columnsf}")
+        raise IndexError(error_message)
+
+
+if __name__ == '__main__':
+    import numpy as np
+    test_columns = ['date', 'open', 'close', 'low', 'high', 'volume']
+    test_df = pd.DataFrame(np.random.rand(4,6), columns=test_columns)
+    
+    # Test core use
+    test_columns_series = pd.Series(test_columns)
+    assert check_dataframe_columns(test_df, test_columns_series) is None
+
+    # test partial match in strict mode
+    test_columns_series = pd.Series(['date', 'open', 'close',
+                                     'low', 'high', 'volume', 'donut'])
+    error_string = None
+    try:
+        check_dataframe_columns(test_df, test_columns_series)
+    except IndexError as e:
+        error_string = str(e)
+
+    assert error_string is not None # tests error
+
+    # test custom error string contains missing column
+    assert 'donut' in error_string
+
+    # test match any coluns: strict = False
+    test_columns_series = pd.Series(['date', 'open', 'close',
+                                     'low', 'high', 'volume', 'donut'])
+    error_string = None
+    try:
+        check_dataframe_columns(test_df, test_columns_series, strict=False)
+    except IndexError as e:
+        error_string = str(e)
+
+    assert error_string is None # tests absence of error
+
+
+        
+
+
+                        
+
+
