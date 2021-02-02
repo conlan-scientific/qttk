9609d4937ba45d2b6a8cd5cfdd4a46aa754d9714
Alex Hernandez <alexh18@vt.edu>
2021-02-02 12:48:08 -0500

    updated function names for bb and macd

--- qttk/indicators.py
+++ qttk/indicators.py
@@ -8,8 +8,8 @@ Production Ready Indicators Interface
   wma
 '''
 from qttk.rsi import compute_net_returns, compute_rsi
-from qttk.bollinger import bollinger, bollinger_demo
-from qttk.macd import macd
+from qttk.bollinger import compute_bb, graph_bb
+from qttk.macd import compute_macd, graph_macd
 from qttk.ma import moving_avg_v4 as compute_ma
 from qttk.cma import cumulative_moving_avg_v2 as compute_cma
 from qttk.ema import exponential_moving_average_v2 as compute_ema
