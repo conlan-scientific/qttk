66c6a6c15bfaf3aca1951f90d7bac7527973f7eb
Alex Hernandez <alexh18@vt.edu>
2021-02-02 12:42:29 -0500

    complete macd

--- qttk/macd.py
+++ qttk/macd.py
@@ -1,22 +1,22 @@
-
 """
 Moving average convergence divergence (MACD) is a trend-following momentum
     indicator that shows the relationship between two moving averages of a security’s
     price. The MACD is calculated by subtracting the 26-period exponential moving
     average (EMA) from the 12-period EMA.
 """
-
 import os
 import sys
 import glob
-import datetime as dt
 from typing import Any, Optional, Iterable
-import pandas as pd
-import matplotlib.pyplot as plt
+import datetime as dt
 import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import pandas as pd
+from progiler import time_this
 from qttk.utils.data_utils import check_dataframe_columns
 
 
+#@time_this
 def compute_macd(eod_data: pd.DataFrame,
                  sm_window: int = 12,
                  lg_window: int = 26) -> pd.DataFrame:
@@ -26,12 +26,17 @@ def compute_macd(eod_data: pd.DataFrame,
          Small Exponential Moving Average(sm_exp)
          Large Exponential Moving Average(lg_exp)
          MACD
+
+         Completed compute_macd in 3.303 milliseconds
        Args:
            eod_data (pd.DataFrame): Open, High, Low, Close Volume dataframe
 
 
        Returns:
-           pd.DataFrame: Columns added, MA_Close, std, BOLU, BOLD, MA_Volume, Bandwidth
+           pd.DataFrame: Columns added, MACD, MACD_MA
+           :param sm_window:
+           :param eod_data:
+           :param lg_window:
        """
 
     sm_exp = eod_data['close'].ewm(span=sm_window,
@@ -42,22 +47,20 @@ def compute_macd(eod_data: pd.DataFrame,
     macd_calc = sm_exp - lg_exp  # obtaining the MACD from subtracting the EMA's
     eod_data['MACD'] = macd_calc  # putting MACD into the dataframe
 
-
     eod_data['MACD_MA'] = macd_calc.rolling(window=9).mean()  # obtaining the moving average of the MACD
-
-    columns_to_fill = ['MACD', 'MACD_MA']
-    eod_data.loc[:, columns_to_fill].fillna(0, inplace=True)
+    eod_data.loc[:, :].fillna(0, inplace=True)
 
     return eod_data
 
 
-def macd_graph_formatter(data_frame: pd.DataFrame) -> None:
-    '''
+#@time_this
+def graph_macd(data_frame: pd.DataFrame) -> None:
+    """
     Relies on global import matplotlib.pyplot as plt
-    '''
+    """
     # macd_data = data_frame[['open', 'close', 'low', 'high']]
-    fig, axs = plt.subplots(3, 1, figsize=(40, 20), gridspec_kw={'height_ratios': [3, 1, 2]})
-    plt.subplots_adjust(hspace=0.4, bottom=0.3)
+    fig, axs = plt.subplots(3, 1, figsize=(20, 10), gridspec_kw={'height_ratios': [3, 1, 2]})
+    plt.subplots_adjust(top=0.947, bottom=0.087, left=0.071, right=0.989, hspace=0.918, wspace=0.2)
 
     # added formatting for axis labels
     locator = mdates.AutoDateLocator(minticks=5, maxticks=30)
@@ -65,8 +68,8 @@ def macd_graph_formatter(data_frame: pd.DataFrame) -> None:
 
     axs[0].set_title('MACD')
     axs[0].plot(data_frame[['MACD', 'MACD_MA']])
-    axs[0].autoscale(enable=True)
-    axs[0].set_xticklabels(data_frame.index, rotation=45)
+    axs[0].xaxis.set_major_locator(locator)
+    axs[0].xaxis.set_major_formatter(formatter)
     axs[0].set_ylabel('MACD')
     axs[0].grid(True)
 
@@ -74,24 +77,28 @@ def macd_graph_formatter(data_frame: pd.DataFrame) -> None:
     axs[1].set_title('MACD')
     axs[1].bar(data_frame.index, data_frame['MACD'])
     axs[1].plot(data_frame.index, data_frame['MACD_MA'], color='black')
-    axs[1].set_xticklabels(data_frame.index, rotation=45)
-    axs[1].set_xlabel('Date')
-    axs[1].set_ylabel('MACD')
+    axs[1].xaxis.set_major_locator(locator)
+    axs[1].xaxis.set_major_formatter(formatter)
+    axs[1].set_ylabel('Volume')
     axs[1].grid(True)
 
     # price Subplot
     axs[2].set_title('Price')
     axs[2].plot(data_frame.index, data_frame['close'], color='black')
-    axs[2].set_xticklabels(data_frame.index, rotation=45)
+    axs[2].xaxis.set_major_locator(locator)
+    axs[2].xaxis.set_major_formatter(formatter)
     axs[2].set_xlabel('Date')
     axs[2].set_ylabel('Price')
     axs[2].grid(True)
 
+    return plt.show()
 
-def macd_demo(data: str = None, data_file_path: Optional[str] = None,
+
+def demo_macd(data: str = None, data_file_path: Optional[str] = None,
               save_figure: bool = False,
               required_columns: Optional[pd.Series] = None) -> None:
     """Main entry ponit for graph generating tool
+
     Args:
         data_file_path (Optional[str], optional): [description]. Defaults to None.
         save_figure (bool, optional): [description]. Defaults to True.
@@ -110,12 +117,12 @@ def macd_demo(data: str = None, data_file_path: Optional[str] = None,
     df = pd.read_csv(csv_file, index_col=0, parse_dates=True)
     df = df.iloc[-180:]  # trim dataframe or autofit axis ?
 
-    # if required_columns is not None:
-    #     check_dataframe_columns(df, required_columns)
+    if required_columns is not None:
+        check_dataframe_columns(df, required_columns)
 
     # df.set_index('date', inplace=True) # set index when csv read
     df = compute_macd(df)
-    macd_graph_formatter(df)
+    graph_macd(df)
     plt.autoscale()
 
     if save_figure:
@@ -130,7 +137,7 @@ if __name__ == '__main__':
     # when the csv is read
     # required_ohlcv_columns = pd.Series(['date', 'open', 'high', 'low', 'close', 'volume'])
     data = 'AWU.csv'  # name of data file to use
-    macd_demo(data, required_columns=required_ohlcv_columns)
+    demo_macd(data, required_columns=required_ohlcv_columns)
 
     # optional loop
     # script_dir = os.path.dirname(__file__)
