375ab83e5bf3f44daae8f9a6441f04d35f888334
Eric Mican <eric@m-i-can.com>
2021-02-04 21:35:44 -0500

    add parse_dats and index_col params to load_sample_data()

--- qttk/utils/sample_data.py
+++ qttk/utils/sample_data.py
@@ -9,28 +9,31 @@ from typing import List
 import pandas as pd
 
 
-def load_sample_data(ticker: str) -> pd.DataFrame:
+def load_sample_data(ticker: str,
+                     parse_dates: bool = True,
+                     index_col: int = 0) -> pd.DataFrame:
     """
     Load sample data from qttk/data/eod directory
 
     Args:
         ticker (str): case insensitive, extension not required
 
-    Returns:
+    Returns (default settings):
         pd.DataFrame: 
-            date       object
-            open      float64
-            close     float64
-            low       float64
-            high      float64
-            volume      int64
+            date      DatetimeIndex
+            open            float64
+            close           float64
+            low             float64
+            high            float64
+            volume            int64
             dtype: object
+
     """
     pwd = os.path.dirname(__file__)
     data_dir = os.path.join(pwd,  '..', 'data', 'eod')
     ticker_csv = os.path.join(data_dir, f'{ticker.upper()}.csv')
     assert os.path.exists(ticker_csv), f"{ticker.upper()}.csv not found"
-    return pd.read_csv(ticker_csv)
+    return pd.read_csv(ticker_csv, parse_dates=parse_dates, index_col=index_col)
 
 
 def list_tickers() -> List[str]:
