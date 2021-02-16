7560caa79632abf3de93f927757180d4c866eec9
joe-wojniak <joe.wojniak@gmail.com>
2021-02-16 11:16:57 -0700

    updated portfolio_optimization.py

--- qttk/examples/portfolio_optimization.py
+++ qttk/examples/portfolio_optimization.py
@@ -25,14 +25,13 @@ if __name__ == '__main__':
      'YPS', 'ZGL']
     # an equally weighted portfolio is assumed
     # weights must add up to 1.0 (100%)
-    weights = np.full((1,len(stocks)), 1/len(stocks))
+    #weights = np.full((1,len(stocks)), 1/len(stocks))
     '''
     Adjusting portfolio weights to demonstrate affects on Sharpe Ratio:
     '''
-    #weights = np.array([[0.125, 0.125, 0.0, 0.125, 0.125, 0.125, 0.0, 0.125,\
-    #0.125, 0.125, 0.0]])
-    #weights = np.array([[0.0, 0.5, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0]])
-    #weights = np.array([[0.0, 0.0, 0.33, 0.0, 0.0, 0.0, 0.33, 0.0, 0.0, 0.0, 0.34]])
+    #weights = np.array([[0.0, 0.0, 0.25, 0.25, 0.25, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0]])
+    #weights = np.array([[0.0, 0.25, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.25, 0.25]])
+    weights = np.array([[0.0, 0.0, 0.33, 0.0, 0.0, 0.0, 0.33, 0.0, 0.0, 0.0, 0.34]])
     portfolio = pd.DataFrame(weights, columns=stocks)
     dataframe = load_portfolio(portfolio.columns.values)
     series = portfolio_price_series(weights, dataframe.iloc[-252:])
@@ -47,23 +46,24 @@ if __name__ == '__main__':
     start = -200 # row to start plotting at, a negative number
     end = -180 # row to end plotting at, a negative number
     plot(returns.iloc[start:end, [11]], test=False, \
-    title='Portfolio Returns', kind='line', legend=False)
+    title='Portfolio Returns', kind='hist', legend=False)
     plot(volatility.iloc[start:end, [11]], test=False, \
-    title='Portfolio Volatility', kind='line', legend=False)
+    title='Portfolio Volatility', kind='hist', legend=False)
 
     # Plot individual stock returns and volitility vs. time
     plot(returns.iloc[start:end, [0, 1, 2, 3, 4, 5]], test=False, \
-    title='Stocks 1-5 Returns', kind='line')
+    title='Stocks 1-5 Returns', kind='hist')
     plot(volatility.iloc[start:end, [0, 1, 2, 3, 4, 5]], test=False, \
-    title='Stocks 1-5 Volatility', kind='line')
+    title='Stocks 1-5 Volatility', kind='hist')
 
     plot(returns.iloc[start:end, [6, 7 , 8, 9, 10]], test=False, \
-    title='Stocks 6-10  Returns', kind='line')
+    title='Stocks 6-10  Returns', kind='hist')
     plot(volatility.iloc[start:end, [6, 7 , 8, 9, 10]], test=False,\
-    title='Stocks 6-10 Volatility', kind='line')
+    title='Stocks 6-10 Volatility', kind='hist')
 
     print('Sharpe Ratio: ', sharpe)
-    print('Portfolio Returns: ', \
-    np.around(returns.iloc[:, [11]].sum(), 5))
+    print('Average Returns: ', \
+    np.around(returns.iloc[:, [11]].mean(), 5))
+    print(dataframe.describe().round(2))
 
     exit
