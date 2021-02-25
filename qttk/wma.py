e74a0cba87fc1da0656f78e088f144a4a31e1ce7
Eric Mican <eric@m-i-can.com>
2021-02-24 21:36:53 -0500

    tidy docstrings for sphinx

--- qttk/wma.py
+++ qttk/wma.py
@@ -1,26 +1,15 @@
 '''
-# Quantitative Trading ToolKit (qttk)
-# https://github.com/conlan-scientific/qttk
-
-testwma.py - Weighted Moving Average Functions
+Profiled and tested weighted moving average functions.
 
 Weighted moving averages assign a heavier weighting to more current
-data points since they are more relevant than dat points in the distant
+data points since they are more relevant than data points in the distant
 past.  The sum of the weighting should add up to 1 (or 100 percent).
 
-from: Investopedia.com
-http://bit.ly/364HCPk
+Weighted moving average is a convolution of data points with a fixed
+weighting function.
 
-# run from project root directory:
-    C:/Users/user/qttk>ipython -i ./qttk/weighted_moving_average.py
-
-Profiled and tested weighted moving average functions
-
-1/31/2021 updates:
-* replaced min_periods parameter name with window for consistency
-* Converted to profiler_v2
-* Implemented function call of profiler_v2
-* Added validation testing
+References:
+    `Investopedia Weighted Moving Average <http://bit.ly/364HCPk>`_
 
 '''
 import pandas as pd
@@ -28,25 +17,23 @@ import numpy as np
 from qttk.profiler_v2 import time_this, timed_report
 from qttk.profiler_v2 import ExponentialRange
 from qttk.utils.data_validation import load_wma_validation_data
-from pandas._testing import assert_almost_equal, assert_series_equal
+from pandas._testing import assert_series_equal
 
 
 def weighted_moving_avg_v1(values: pd.Series,
                            window: int = 5) -> pd.Series:
-    '''
-    Description:
-      Backwards looking weighted moving average.
+    """
+    Weighted Moving Average used for benchmarking.  This implementation
+    uses a pandas series with a for loop to assign the weighted moving average.
 
-    Complexity:
-      O(nm) because n values * window sized window
+    Args:
+        values (pd.Series): price series
+        window (int, optional): Defaults to 5.
 
-    Memory Usage:
+    Returns:
+        pd.Series: weighted moving average named wma
+    """
 
-    Inputs:
-      values: pd.Series of closing prices
-      m     : period to be calculated
-
-    '''
     # Constant
     weights = pd.Series(range(1, window+1))
     weights = weights / weights.sum()
@@ -56,7 +43,7 @@ def weighted_moving_avg_v1(values: pd.Series,
         return pd.Series([np.nan]*len(values))
 
     # initialize and copy index from input series for timeseries index
-    wma = pd.Series([np.nan]*values.shape[0], index=values.index)
+    wma = pd.Series([np.nan]*values.shape[0], index=values.index, name='wma')
 
     for i in range(window, values.shape[0]+1):
         wma.iloc[i-1] = (values.iloc[i-window:i].values * weights.values).sum()
@@ -66,13 +53,20 @@ def weighted_moving_avg_v1(values: pd.Series,
 
 def weighted_moving_avg_v2(values: pd.Series,
                            window: int = 5) -> pd.Series:
-    '''
-    Description:
-      Backwards looking weighted moving average.
-    Complexity:
-      O(n)?  This is the slowest method, hidden complexity?
-    Memory Usage:
-    '''
+    """
+    Weighted Moving Average used for benchmarking.  This implementation is
+    using two pd.series like v1 and has additional feature: it deletes the
+    window of values from the original series in an attempt to reduce memory.
+    Index operations add drag and the function performs worse than v1.
+
+    Args:
+        values (pd.Series): price series
+        window (int, optional): Defaults to 5.
+
+    Returns:
+        pd.Series: weighted moving average series
+    """
+
     # Constant
     weights = pd.Series(range(1, window+1))
     weights = weights / weights.sum()
@@ -82,7 +76,7 @@ def weighted_moving_avg_v2(values: pd.Series,
         return pd.Series([np.nan]*len(values))
 
     # initialize series and copy index from input series to return a matching index
-    wma = pd.Series([np.nan]*values.shape[0], index=values.index)
+    wma = pd.Series([np.nan]*values.shape[0], index=values.index, name='wma')
     group = values.iloc[0:window-1]
 
     for idx in values.iloc[window-1:].index:
@@ -92,21 +86,17 @@ def weighted_moving_avg_v2(values: pd.Series,
 
     return wma
 
+
 def _np_weighted_moving_avg(values: np.array, window: int = 5) -> np.array:
     '''
-    np convolution method
-
-    Description:
-      Backwards looking weighted moving average with numpy.
-
-    Complexity:
-      O(n) because n values
-
-    Memory Usage:
+    Implementation of np.convolve weighted moving average.  'valid' is in
+    context of signal processing and simply means to only compute values
+    where the series overlap.  Full and Same options return an array with
+    a shape greater than the input array.
 
     References:
-      https://numpy.org/doc/stable/reference/generated/numpy.convolve.html
-      https://en.wikipedia.org/wiki/Convolution
+        https://numpy.org/doc/stable/reference/generated/numpy.convolve.html
+        https://en.wikipedia.org/wiki/Convolution
 
     '''
     weights = np.arange(1, window + 1)
@@ -115,26 +105,37 @@ def _np_weighted_moving_avg(values: np.array, window: int = 5) -> np.array:
 
 
 def weighted_moving_avg_v3(values: pd.Series, window: int = 5) -> pd.Series:
-    '''
-    Wrapper to use np_weighted_moving_avg function
-    .00257 ÷ .000706 = 3.64x slower than talib.WMA with arguments:
-        series = pd.Series(np.random.random((1000 * 100)))
-        window = 12
-    '''
+    """
+    Backwards looking weighted moving average implemented with Numpy's
+    convolution method. As the window changes, the weighted series is applied
+    to different parts of the input series.
+
+    References:
+        https://numpy.org/doc/stable/reference/generated/numpy.convolve.html
+        https://en.wikipedia.org/wiki/Convolution
+
+    Args:
+        values (pd.Series): price series
+        window (int, optional): Defaults to 5.
+
+    Returns:
+        pd.Series: weighted moving average series
+
+    """
 
     # when period is greater than values, return
     if values.shape[0] <= window:
         return pd.Series([np.nan]*len(values))
 
     # initialize series and copy index from input series to return a matching index
-    wma = pd.Series([np.nan]*values.shape[0], index=values.index)
+    wma = pd.Series([np.nan]*values.shape[0], index=values.index, name='wma')
     wma.iloc[window-1:] = _np_weighted_moving_avg(values.values, window)
+
     return wma
 
 
-
 if __name__ == '__main__':
-    #validation testing
+    # validation testing
     data, target = load_wma_validation_data()
     window = 20
 
@@ -147,7 +148,7 @@ if __name__ == '__main__':
     v3 = weighted_moving_avg_v3(data, window=window)
     assert_series_equal(v3, target, check_names=False)
     
-
+    # performance profiling
     exp_range = ExponentialRange(1, 4, 1/4)
     series = pd.Series(np.random.random(exp_range.max))
 
@@ -162,4 +163,3 @@ if __name__ == '__main__':
 
         for i in exp_range.iterator():
             tt(weighted_moving_avg_v3)(series.iloc[:i], window=20)
-
